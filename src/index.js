0d2adf3889011983a72691f63a9b4467ebb7ecbf
joaquingutierrez <joaquinguty@gmail.com>
2023-03-16 10:19:55 -0300

    Ventana de escritorio funcionando

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const { createWindow } = require("./main")
+const { app } = require("electron")
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
